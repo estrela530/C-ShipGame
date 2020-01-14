a077567ec5f90ab605934b62c6de609144533f5d
estrela530 <daisuke.0530.otk@gmail.com>
2020-01-14 13:39:17 +0900

    Play画面

--- ShipGameC++/ShipGame/GamePlay.cpp
+++ ShipGameC++/ShipGame/GamePlay.cpp
@@ -1,33 +1,22 @@
 #include "GamePlay.h"
 #include "GraphFactory.h"
-#include "Window.h"
 
 //	初期化
 void GamePlay::Initialize()
 {
-	//各オブジェクトを初期化
-	_player.Start();
-	
+	//	ゲーム画面の背景画像読み込み
+	_gameImage = GraphFactory::Instance().LoadGraph("img\\backcolor.png");
 }
 
 //	更新
 void GamePlay::Update()
 {
-	//更新処理を実行
-	_player.Update();
-	
-	//描画処理を実行
-	
-	_player.Render();
-
-#ifdef _DEBUG_
-	
-#endif // _DEBUG_
-
-
+	//	ゲーム画面を表示する
+	DrawGraph(0, 0, _gameImage, FALSE);
 }
 
 //	解放
 void GamePlay::Release()
 {
+	GraphFactory::Instance().EraseGraph("img\\backcolor.png");
 }
